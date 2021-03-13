5a62ea7365e09a7cce4be85bc04a45812d80f0c1
Nabil <mahabubahmednabil@gmail.com>
2021-03-13 23:40:03 +0600

    Component Tree

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import MainComponent from './Components/MainComponent';
 
 function App() {
   return (
     <div className="App">
-
+      <MainComponent />
     </div>
   );
 }
