049bdf4ec6512d1d269a76ba1f5602f0b14e309e
Nabil <mahabubahmednabil@gmail.com>
2021-04-15 12:17:18 +0600

    Adding Routes

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -3,11 +3,15 @@ import Menu from './Menu';
 import About from './About';
 import Contact from './Contact';
 import Home from './Home';
+import { Route, Redirect } from 'react-router-dom';
 
 const Body = () => {
     return (
         <div>
-
+            <Route path="/" exact component={Home} />
+            <Route path="/menu" exact component={Menu} />
+            <Route path="/about" exact component={About} />
+            <Route path="/contact" exact component={Contact} />
         </div>
     );
 }
