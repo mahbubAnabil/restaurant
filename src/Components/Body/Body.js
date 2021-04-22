3ff385b3b9a68b18c0ddea9639102db24859defd
Nabil <mahabubahmednabil@gmail.com>
2021-04-22 11:55:39 +0600

    creating contact form

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -3,15 +3,18 @@ import Menu from './Menu';
 import About from './About';
 import Contact from './Contact';
 import Home from './Home';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 const Body = () => {
     return (
         <div>
-            <Route path="/" exact component={Home} />
-            <Route path="/menu" exact component={Menu} />
-            <Route path="/about" exact component={About} />
-            <Route path="/contact" exact component={Contact} />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/menu" exact component={Menu} />
+                <Route path="/about" exact component={About} />
+                <Route path="/contact" exact component={Contact} />
+                <Redirect from="/" to="/home" />
+            </Switch>
         </div>
     );
 }
